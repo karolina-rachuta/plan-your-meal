7515d44ad6cbb075d79897577e20e62868b04ac9
Karolina Rachuta <karolina.rachuta@gmail.com>
2025-01-14 19:24:55 +0400

    working on hero section

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Header from '../components/Header';
+import Header from '../components/homepage/Header';
+import Hero from '../components/homepage/Hero';
 
 function Homepage() {
     return (
         <div>
             <Header />
+            <Hero />
             {/* <div>karuzela</div>
             <div>cta</div>
             <div>formularz do zapisy</div>
