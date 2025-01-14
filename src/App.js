3e38dfa408b108ee79e9ebeadcf7fc2f2e5af8de
Karolina Rachuta <karolina.rachuta@gmail.com>
2025-01-14 13:22:58 +0400

    Adding fonts, colors, react-router, variables

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
 
 function App() {
-    return <div className="App">hello </div>;
+    return (
+        <div className="container">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<div>Hello</div>} />
+                    <Route path="/app" element={<div>app</div>} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
