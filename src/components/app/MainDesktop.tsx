2c1bc6f43c3344d0608e76ca0f7b9174032ff85e
Karolina Rachuta <karolina.rachuta@gmail.com>
2026-04-02 18:13:45 +0100

    moving meal plan navigation, reorganization

--- src/components/app/MainDesktop.tsx
+++ src/components/app/MainDesktop.tsx
@@ -34,99 +34,119 @@ function MainDesktop({ onScreenChange }: Props) {
         );
     }
     return (
-        <div className="maindesktop__container">
-            <div className="maindesktop__top">
-                <div className="maindesktop__widget">
-                    <button
-                        className="widget__box"
-                        onClick={() => onScreenChange(4)}
-                    >
-                        <div className="widget__border">
+        <div className="container">
+            <div className="maindesktop__container">
+                <div className="maindesktop__top">
+                    <div className="maindesktop__widget">
+                        <button
+                            className="widget__box"
+                            onClick={() => onScreenChange(4)}
+                        >
+                            <div className="widget__border">
+                                <img
+                                    src={Plus}
+                                    alt="Plus"
+                                    className="widget__add"
+                                />
+                            </div>
+                            Add Recipe
+                        </button>
+                        <button
+                            className="widget__box"
+                            onClick={() => onScreenChange(5)}
+                        >
+                            <div className="widget__border widget__border--right">
+                                <img
+                                    src={Plus_orange}
+                                    alt="Plus"
+                                    className="widget__add"
+                                />
+                            </div>
+                            Add Meal Plan
+                        </button>
+                    </div>
+                    <div className="maindesktop__widget maindesktop__widget--col">
+                        <div className="widget__box widget__box--right widget__box--green">
                             <img
-                                src={Plus}
-                                alt="Plus"
-                                className="widget__add"
+                                src={Information}
+                                alt="Information"
+                                className="widget__add--right"
                             />
+                            You have {totalRecipes} recipes
                         </div>
-                        Add Recipe
-                    </button>
-                    <button
-                        className="widget__box"
-                        onClick={() => onScreenChange(5)}
-                    >
-                        <div className="widget__border widget__border--right">
+                        <div className="widget__box widget__box--right">
                             <img
-                                src={Plus_orange}
-                                alt="Plus"
-                                className="widget__add"
+                                src={Exclamation}
+                                alt="Exclamation"
+                                className="widget__add--right"
                             />
+                            Remember to add a meal plan!
                         </div>
-                        Add Meal Plan
-                    </button>
-                </div>
-                <div className="maindesktop__widget maindesktop__widget--col">
-                    <div className="widget__box widget__box--right widget__box--green">
-                        <img
-                            src={Information}
-                            alt="Information"
-                            className="widget__add--right"
-                        />
-                        You have {totalRecipes} recipes
-                    </div>
-                    <div className="widget__box widget__box--right">
-                        <img
-                            src={Exclamation}
-                            alt="Exclamation"
-                            className="widget__add--right"
-                        />
-                        Remember to add a meal plan!
-                    </div>
-                    <div className="widget__box widget__box--right widget__box--green">
-                        <img
-                            src={Check}
-                            alt="Check"
-                            className="widget__add--right"
-                        />{' '}
-                        So glad you&apos;re here! Happy planning and bon
-                        appétit!
+                        <div className="widget__box widget__box--right widget__box--green">
+                            <img
+                                src={Check}
+                                alt="Check"
+                                className="widget__add--right"
+                            />{' '}
+                            So glad you&apos;re here! Happy planning and bon
+                            appétit!
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="maindesktop__container add__container">
-                {selectedPlan && (
-                    <>
-                        <h4 className="mealPlan__title">
-                            Your Meal Plan for : {selectedPlan.number} week
-                        </h4>
-                        <div className="add__botom">
-                            {Object.keys(selectedPlan.mealPlan).map((day) => (
-                                <div className="mealplan__table" key={day}>
-                                    <h4 className="mealPlan__header">{day}</h4>
-                                    {Object.entries(
-                                        selectedPlan.mealPlan[
-                                            day as keyof WeekSchedule
-                                        ]
-                                    ).map(([key, value]) => (
-                                        <p
-                                            className="mealPlan__row"
-                                            key={`${day}-${key}`}
-                                        >
-                                            {value}
-                                        </p>
-                                    ))}
+                <div className="maindesktop__container add__container">
+                    {selectedPlan && (
+                        <>
+                            <div className="mealPlan__titleBox">
+                                <h4 className="mealPlan__title">
+                                    Your Meal Plan for :{' '}
+                                    <span className="mealPlan__title mealPlan__title--span">
+                                        {selectedPlan.number} week
+                                    </span>
+                                </h4>
+                                <div className="maindesktop-navigation">
+                                    <button
+                                        onClick={handlePrev}
+                                        className="btn"
+                                    >
+                                        &lt;
+                                    </button>
+                                    <button
+                                        onClick={handleNext}
+                                        className="btn"
+                                    >
+                                        &gt;
+                                    </button>
                                 </div>
-                            ))}
-                        </div>
-                        <div className="maindesktop-navigation">
-                            <button onClick={handlePrev} className="btn">
-                                Prev
-                            </button>
-                            <button onClick={handleNext} className="btn">
-                                Next
-                            </button>
-                        </div>
-                    </>
-                )}
+                            </div>
+                            <div className="add__botom">
+                                {Object.keys(selectedPlan.mealPlan).map(
+                                    (day) => (
+                                        <div
+                                            className="mealplan__table"
+                                            key={day}
+                                        >
+                                            <h4 className="mealPlan__header">
+                                                {day}
+                                            </h4>
+                                            {Object.entries(
+                                                selectedPlan.mealPlan[
+                                                    day as keyof WeekSchedule
+                                                ]
+                                            ).map(([key, value]) => (
+                                                <p
+                                                    className="mealPlan__row"
+                                                    key={`${day}-${key}`}
+                                                >
+                                                    {value}
+                                                </p>
+                                            ))}
+                                        </div>
+                                    )
+                                )}
+                            </div>
+                        </>
+                    )}
+                </div>
             </div>
         </div>
     );
